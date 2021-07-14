9a41da268942c03f156ebac8658fe1f23180fd59
Mayssa Jawadi <mayssajawadip4@gmail.com>
2021-07-14 05:15:46 +0100

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/articles', function () {
+    echo "Im Mayssa";
+});
+
+Route::get('/user/{name}', function ($name) {
+    return "Good Morning {$name}";
+   });
+
+   Route::get('/user/{name}/{age}', function ($name, $age) {
+    return "<h1>I am {$name} and my age is {$age}</h1>";
+   });
+
+   Route::get('/user/{name}/{age}/{city}', function ($name, $age, $city) {
+    return "<b>I am {$name} and my age is {$age} and I'm live in  {$city}</b>";
+   });
+
+   Route::get('/divide/{a}/{b}', function ($a, $b) {
+    return  $a + $b;
+   });
